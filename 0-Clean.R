16f248e5a247ed0342c04841211e9896f1ab146f
bpales <bpales1@lsu.edu>
2016-02-11 19:30:00 -0600

    ####

--- 0-Clean.R
+++ 0-Clean.R
@@ -9,5 +9,5 @@ storms <- storms  %>%
   mutate(time = ymd_h(paste(year, month, day, hour))) %>%
   select(name,year,time,lat,long,pressure,wind,type)
 write.csv(storms, file="storms.csv", row.names = FALSE)
-
+##################
 
